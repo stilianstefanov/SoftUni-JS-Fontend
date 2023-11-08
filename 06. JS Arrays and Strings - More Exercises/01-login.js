2a722ddf4e4be8491d92573efe120f0fb866eab7
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-11-08 13:07:34 +0200

    Add files via upload

---
+++ 06. JS Arrays and Strings - More Exercises/01-login.js
@@ -0,0 +1,19 @@
+function solve(passwords) {
+    let username = passwords.shift();
+    let counter = 1;
+
+    passwords.forEach(p => {
+        if(p === username.split('').reverse().join('')) {
+            console.log(`User ${username} logged in.`);
+            return;
+        }
+        else if(counter === 4){
+            console.log(`User ${username} blocked!`);
+            return;
+        }
+        else {
+            console.log(`Incorrect password. Try again.`);
+            counter++;
+        }
+    });
+}
