f13fb8d94f70f1de9b8717c3e08c6ec1204f6f18
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-11-11 16:12:58 +0200

    Add files via upload

---
+++ 08. Functions and Statements - Exercises/05-palindrome-Integers.js
@@ -0,0 +1,21 @@
+function palindromeCheck(numbers) {
+    for (let i = 0; i < numbers.length; i++) {
+        let currentNum = numbers[i].toString();
+        let firstHalf = [];
+        let secondHalfReversed = [];
+
+        if (currentNum.length % 2 === 0) {
+            firstHalf = currentNum.split('').slice(0, currentNum.length / 2);
+            secondHalfReversed = currentNum.split('').slice(currentNum.length / 2).reverse();
+        }
+        else {
+            firstHalf = currentNum.split('').slice(0, Math.floor(currentNum.length / 2));
+            secondHalfReversed = currentNum.split('').slice(Math.ceil(currentNum.length / 2)).reverse();
+        }
+        
+        let isPalindrome = firstHalf.every((value, index) => value === secondHalfReversed[index]);
+        isPalindrome ? console.log('true') : console.log('false');
+    }
+}
+
+palindromeCheck([32,2,232,1010]);
