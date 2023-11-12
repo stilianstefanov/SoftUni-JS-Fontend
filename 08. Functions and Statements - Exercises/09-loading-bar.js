4a65913d86fb429266245725b53c5ca99383cf42
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-11-12 18:46:18 +0200

    Add files via upload

---
+++ 08. Functions and Statements - Exercises/09-loading-bar.js
@@ -0,0 +1,19 @@
+function solve(num) {
+    if (num === 100) {
+        console.log('100% Complete!');
+        return;
+    }
+
+    let bar = [];
+    for (let i = 0; i < 10; i++) {
+        if (i < num / 10) {
+            bar.push('%');
+        }
+        else {
+            bar.push('.');
+        }
+    }
+
+    console.log(`${num}% [${bar.join('')}]`);
+    console.log('Still loading...');
+}
