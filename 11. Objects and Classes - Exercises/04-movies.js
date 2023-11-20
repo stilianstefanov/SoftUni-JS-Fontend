d3f7963f278a377c4c6c0c91c35aaf2b8752f7f0
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-11-20 13:46:21 +0200

    Add files via upload

---
+++ 11. Objects and Classes - Exercises/04-movies.js
@@ -0,0 +1,35 @@
+function solve(input) {
+    let moviesStore = [];
+
+    for (const command of input) {
+        if (command.includes('addMovie')) {
+            moviesStore.push({ name: command.slice(9) });
+        }
+        else if (command.includes('directedBy')) {
+            const movieName = command.slice(0, command.indexOf('directedBy') - 1);
+            const movieDirector = command.slice(command.indexOf('directedBy') + 11);
+
+            for (const movie of moviesStore) {
+                if (movie.name === movieName) {
+                    movie['director'] = movieDirector;
+                }
+            }
+        }
+        else {
+            const movieName = command.slice(0, command.indexOf('onDate') - 1);
+            const movieDate = command.slice(command.indexOf('onDate') + 7);
+
+            for (const movie of moviesStore) {
+                if (movie.name === movieName) {
+                    movie['date'] = movieDate;
+                }
+            }
+        }
+    }
+
+    for (const movie of moviesStore) {
+        if (movie.hasOwnProperty('director') && movie.hasOwnProperty('date')) {
+            console.log(JSON.stringify(movie));
+        }
+    }
+}
