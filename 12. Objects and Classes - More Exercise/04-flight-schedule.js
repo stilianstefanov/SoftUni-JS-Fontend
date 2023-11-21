3d9d63bbac2adc003dff2da65ee2c52590fbf57a
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-11-21 16:34:59 +0200

    Add files via upload

---
+++ 12. Objects and Classes - More Exercise/04-flight-schedule.js
@@ -0,0 +1,31 @@
+function solve(inputArr) {
+    const flightsInfo = inputArr[0];
+    const newStatuses = inputArr[1];
+    const statusToCheck = inputArr[2];
+
+    let flights = {};
+    for (const curFlight of flightsInfo) {
+        const [flightNo, destination] = curFlight.split(' ');
+        flights[flightNo] = {
+            destination,
+            status: 'Ready to fly'
+        };
+    }
+    for (const statusInfo of newStatuses) {
+        const [flightNo, newStatus] = statusInfo.split(' ');
+        if (flights.hasOwnProperty(flightNo)) flights[flightNo].status = newStatus;
+    }
+
+    if (statusToCheck == 'Ready to fly') {
+        for (const key in flights) {
+            if (flights[key].status === 'Ready to fly')
+                console.log(`{ Destination: '${flights[key].destination}', Status: '${flights[key].status}' }`);
+        }
+    }
+    else {
+        for (const key in flights) {
+            if (flights[key].status !== 'Ready to fly')
+                console.log(`{ Destination: '${flights[key].destination}', Status: '${flights[key].status}' }`);
+        }
+    }
+}
