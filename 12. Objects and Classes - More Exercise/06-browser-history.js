5e648af0626d46399277ae750027f82150f3aef9
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-11-22 14:11:44 +0200

    Add files via upload

---
+++ 12. Objects and Classes - More Exercise/06-browser-history.js
@@ -0,0 +1,45 @@
+function solve(browserObj, commandArr) {
+    for (const commandInfo of commandArr) {
+        const [command, website] = commandInfo.split(' ');
+
+        if (command === 'Open') {
+            browserObj['Open Tabs'].push(website);
+        }
+        else if (command === 'Close') {
+            if (!browserObj['Open Tabs'].includes(website)) continue;
+
+            const index = browserObj['Open Tabs'].indexOf(website);
+            browserObj['Open Tabs'].splice(index, 1);
+            browserObj['Recently Closed'].push(website);
+        }
+        else {
+            browserObj['Open Tabs'] = [];
+            browserObj['Recently Closed'] = [];
+            browserObj['Browser Logs'] = [];
+            continue;
+        }
+        browserObj['Browser Logs'].push(commandInfo);
+    }
+    console.log(browserObj['Browser Name']);
+    console.log(`Open Tabs: ${browserObj['Open Tabs'].join(', ')}`);
+    console.log(`Recently Closed: ${browserObj['Recently Closed'].join(', ')}`);
+    console.log(`Browser Logs: ${browserObj['Browser Logs'].join(', ')}`);
+}
+
+
+
+
+
+solve({"Browser Name":"Mozilla Firefox",
+
+"Open Tabs":["YouTube"],
+
+"Recently Closed":["Gmail",
+
+"Dropbox"],
+
+"Browser Logs":["Open Gmail",
+
+"Close Gmail", "Open Dropbox", "Open YouTube", "Close Dropbox"]},
+
+["Open Wikipedia", "Clear History and Cache", "Open Twitter"])
